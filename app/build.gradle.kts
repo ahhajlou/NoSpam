53e8b4ebcbe26eefe84fa96f08d6165ac8a5105d
Amirhossein Hajlou <hajlouamirhossein@gmail.com>
2026-09-10 16:43:58 +0330

    Bump version to v0.1.0-alpha.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nospam.nospam"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "v0.1.0-alpha.1"
+        versionCode = 2
+        versionName = "v0.1.0-alpha.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
