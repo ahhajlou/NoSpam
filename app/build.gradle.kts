70f9ccede2c7b69ba5ea181e7541b1863582c486
Amirhossein Hajlou <61903474+ahhajlou@users.noreply.github.com>
2026-09-25 02:05:08 +0330

    chore: bump version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 }
 
-val appVersionName = "0.3.0"
+val appVersionName = "0.3.1"
 
 // Release signing material never lives in this repository. It is read from
 // keystore.properties at the root (gitignored, see keystore.properties.template)
@@ -53,7 +53,7 @@ android {
         applicationId = "com.nospam.nospam"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
+        versionCode = 10
         versionName = appVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
